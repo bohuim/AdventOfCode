348dfdc15083fa09ca3bc6e8892e281bea6094f4
Bohui Moon <bohuimoon@gmail.com>
2020-12-08 20:07:48 -0800

    y2020d8p2

--- 2020/src/day8.rs
+++ 2020/src/day8.rs
@@ -10,21 +10,23 @@ lazy_static! {
 type Program = Vec<Inst>;
 
 /// Represents the instruction set.
-#[derive(PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Inst {
   ACC(i64),
   JMP(i64),
-  NOP,
+  NOP(i64),
 }
 impl Inst {
   /// Parses an instruction from a line.
-  fn from(line: &str) -> Option<Inst> {
-    RE_INST.captures(line.trim())
-      .map(|groups| match groups.get(1).unwrap().as_str() {
-        "acc" => Inst::ACC(groups.get(2).unwrap().as_str().parse().unwrap()),
-        "jmp" => Inst::JMP(groups.get(2).unwrap().as_str().parse().unwrap()),
-        _ => Inst::NOP,
-      })
+  fn from(line: &str) -> Inst {
+    let groups = RE_INST.captures(line.trim()).unwrap();
+    let arg: i64 = groups.get(2).unwrap().as_str().parse().unwrap();
+    match groups.get(1).unwrap().as_str() {
+      "acc" => Inst::ACC(arg),
+      "jmp" => Inst::JMP(arg),
+      "nop" => Inst::NOP(arg),
+      _ => panic!("Unknown instruction opcode"),
+    }
   }
 }
 
@@ -43,7 +45,7 @@ fn run(program: &Program) -> (bool, i64) {
         acc += add;
         i + 1
       },
-      Inst::NOP => i + 1,
+      Inst::NOP(_) => i + 1,
     };
     if i == last {
       return (true, acc);
@@ -52,9 +54,22 @@ fn run(program: &Program) -> (bool, i64) {
   (false, acc)
 }
 
+/// Creates a modified program by swapping jmp <-> nop instructions at the given line `i`.
+/// 
+/// **Note**: Returns a copy of the program if not a jmp or nop instruction.
+fn modified(original: &Program, i: usize) -> Program {
+  let mut copied: Program = original.to_vec();
+  copied[i] = match copied[i] {
+    Inst::JMP(arg) => Inst::NOP(arg),
+    Inst::NOP(arg) => Inst::JMP(arg),
+    _ => copied[i].to_owned(),
+  };
+  copied
+}
+
 #[aoc_generator(day8)]
 pub fn parse(input: &str) -> Program {
-  input.trim().lines().filter_map(Inst::from).collect()
+  input.trim().lines().map(Inst::from).collect()
 }
 
 #[aoc(day8, part1)]
@@ -64,12 +79,28 @@ pub fn solve1(program: &Program) -> i64 {
   acc
 }
 
+#[aoc(day8, part2)]
+pub fn solve2(program: &Program) -> i64 {
+  (0..program.len())
+    // filter for instruction lines that are jmp or nop
+    .filter(|&i| match program[i] {
+      Inst::JMP(_) | Inst::NOP(_) => true,
+      _ => false,
+    })
+    // swap jmp <-> nop opcodes at the line and run
+    .map(|i| run(&modified(&program, i)))
+    // find the first modified program that completes
+    .find(|&(completed, _)| completed)
+    // get ending acc value of the modified program
+    .unwrap().1
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
 
   lazy_static! {
-    static ref SAMPLE1: Program = parse(r"
+    static ref SAMPLE: Program = parse(r"
       nop +0
       acc +1
       jmp +4
@@ -84,15 +115,19 @@ mod tests {
 
   #[test]
   fn inst_from() {
-    assert_eq!(Inst::from("acc +42").unwrap(), Inst::ACC(42));
-    assert_eq!(Inst::from("jmp -13").unwrap(), Inst::JMP(-13));
-    assert_eq!(Inst::from("nop +0").unwrap(), Inst::NOP);
-    assert_eq!(Inst::from("nop -0").unwrap(), Inst::NOP);
-    assert_eq!(Inst::from("abc -0"), None);
+    assert_eq!(Inst::from("acc +42"), Inst::ACC(42));
+    assert_eq!(Inst::from("jmp -13"), Inst::JMP(-13));
+    assert_eq!(Inst::from("nop +0"), Inst::NOP(0));
+    assert_eq!(Inst::from("nop -0"), Inst::NOP(0));
   }
 
   #[test]
-  fn solve1_sample1() {
-    assert_eq!(solve1(&SAMPLE1), 5);
+  fn solve1_sample() {
+    assert_eq!(solve1(&SAMPLE), 5);
+  }
+
+  #[test]
+  fn solve2_sample() {
+    assert_eq!(solve2(&SAMPLE), 8);
   }
 }
